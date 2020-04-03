f1d448c198be4b535d4bd96c009264532475f9dd
Karimah <Y.Karimah@cgiar.org>
2020-04-03 18:37:18 +0700

    Try to run the code

--- app.R
+++ app.R
@@ -17,7 +17,7 @@ library(koboloadeR)
 # library(rtf)
 library(sodium)
 
-loginpage <- div(id = "loginpage", style = "width: 500px; max-width: 100%; margin: 0 auto; padding: 20px;",
+loginpage <- div(titlePanel ("VAM PRK"), id = "loginpage", style = "width: 500px; max-width: 100%; margin: 0 auto; padding: 20px;",
                  wellPanel(
                    tags$h2("Log In", class = "text-center", style = "padding-top: 0;color:#333; font-weight:600;"),
                    textInput("userName", placeholder="Email", label = tagList(icon("user"), "Email")),
